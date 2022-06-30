9002fc0d4ba011d0a53f888776ae102e12acee8c
Mihai Voina <mihaitza1058@yahoo.com>
2022-06-30 09:54:29 +0300

    BridgeDesignPattern

---
+++ Mihai/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+
+namespace BridgeDesignPattern
+{
+    public interface LEDTV
+    {
+        void SwitchOn();
+        void SwitchOff();
+        void SetChannel(int channelNumber);
+    }
+
+    public class SamsungLedTv : LEDTV
+    {
+        public void SwitchOn()
+        {
+            Console.WriteLine("Turning ON : Samsung TV");
+        }
+        public void SwitchOff()
+        {
+            Console.WriteLine("Turning OFF : Samsung TV");
+        }
+        public void SetChannel(int channelNumber)
+        {
+            Console.WriteLine("Setting channel Number " + channelNumber + " on Samsung TV");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SonyRemoteControl sonyRemoteControl = new SonyRemoteControl(new SonyLedTv());
+            sonyRemoteControl.SwitchOn();
+            sonyRemoteControl.SetChannel(101);
+            sonyRemoteControl.SwitchOff();
+
+            Console.WriteLine();
+            SamsungRemoteControl samsungRemoteControl = new SamsungRemoteControl(new SamsungLedTv());
+            samsungRemoteControl.SwitchOn();
+            samsungRemoteControl.SetChannel(202);
+            samsungRemoteControl.SwitchOff();
+
+            Console.ReadKey();
+        }
+    }
+
+}
+
+
